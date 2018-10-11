d3ac868dfb9868abb7a9b00fd57c73bbd3160bdb
Cong-Yun <1045300328@qq.com>
2018-10-11 16:57:48 +0800

    Initial commit

---
+++ src/main/java/com/imooc/service/PushMessageService.java
@@ -0,0 +1,10 @@
+package com.imooc.service;
+
+import com.imooc.dto.OrderDTO;
+
+
+public interface PushMessageService {
+
+
+    void orderStatus(OrderDTO orderDTO);
+}
---
+++ src/main/java/com/imooc/service/SellerService.java
@@ -0,0 +1,10 @@
+package com.imooc.service;
+
+import com.imooc.dataobject.SellerInfo;
+
+
+public interface SellerService {
+
+
+    SellerInfo findSellerInfoByOpenid(String openid);
+}
